96214cafe9fedd3361e468060f6df39bec4c8a83
KHAGESWAR <88511621+khageswar9@users.noreply.github.com>
2022-03-28 10:49:45 +0530

    third

---
+++ src/controllers/todo.controller.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+const todo  = require("../models/todo.model");
+const authenticate = require("../middleware/authenticate.middleware");
+
+
+router.post("", authenticate, async (req, res) => {
+
+    req.body.user_id = req.userID;
+    try{
+        const todo = await todo.create(req.body)
+        return res.status(200).send(todo);
+    }
+    catch(err){
+        return res.status(400).send({message : err.message})
+    }
+ 
+});
+router.get("", async (req, res) => {
+
+    req.body.user_id = req.userID;
+    try{
+        const todo = await todo.find().lean().excle()
+        return res.status(200).send(todo);
+    }
+    catch(err){
+        return res.status(400).send({message : err.message})
+    }
+ 
+});
+router.delete("/:id", async (req, res) => {
+
+    try{
+        const todo = await todo.findByIdAndDelete(req.params.id);
+        return res.status(200).send(todo);
+    }
+    catch(err){
+        return res.status(400).send({message : err.message})
+    }
+ 
+});
